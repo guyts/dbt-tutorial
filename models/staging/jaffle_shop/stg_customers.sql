d270fdc302947852427ad88e4eb4a78174a7bffa
guytsror@gmail.com <guytsror@gmail.com>
2021-10-03 18:04:54 +0000

    added sources

--- models/staging/jaffle_shop/stg_customers.sql
+++ models/staging/jaffle_shop/stg_customers.sql
@@ -2,4 +2,5 @@ select
     cast(id as int64) as customer_id,
     first_name,
     last_name
-from dbt-tutorial.jaffle_shop.customers
+
+from {{ source('jaffle_shop', 'customers') }}
--- models/staging/jaffle_shop/stg_orders.sql
+++ models/staging/jaffle_shop/stg_orders.sql
@@ -1,8 +1,7 @@
-
 select
     id as order_id,
     cast(user_id as int64) as customer_id,
     order_date,
     status
 
-from dbt-tutorial.jaffle_shop.orders
+from {{ source('jaffle_shop', 'orders') }}
